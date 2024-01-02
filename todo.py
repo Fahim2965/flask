827b0f88a49fd062f55a05965977ee23632bca85
Fahim Muntasir <famuntasir2@brooklynsteamcenter.org>
2024-01-02 10:50:59 -0500

    do more work

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ todos = ["do work", "do more work", "and then do some more work"]
 
 @app.route('/', methods =['GET', 'POST'])
 def index():
-    new_todo = request.form["new_todo"]
-    todos.append(new_todo)
+    if request.method == 'POST':
+        new_todo = request.form["new_todo"]
+        todos.append(new_todo)
     return render_template("todo.html.jinja", my_todos = todos)
+
+
+@app.route('/delete_todo/<int:todo_index>', methods = ['POST'])
+def todo_delete(todo_index):
+    del todos[todo_index]
+
+    return redirect('/')
