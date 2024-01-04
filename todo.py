442d38a7edd723a8a9183a70544460fd50807b35
Fahim Muntasir <famuntasir2@brooklynsteamcenter.org>
2024-01-04 10:52:45 -0500

    now do more work

--- todo.py
+++ todo.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, redirect
+import pymysql
+import pymysql.cursors
+from pprint import pprint as print
+
 
 app = Flask(__name__)
 
@@ -6,10 +10,23 @@ todos = ["do work", "do more work", "and then do some more work"]
 
 @app.route('/', methods =['GET', 'POST'])
 def index():
+
+    conn = pymysql.connect(
+    database = "fmuntasir2_todos",
+    user = "fmuntasir2",
+    password = "239442965",
+    host = "10.100.33.60",
+    cursorclass = pymysql.cursors.DictCursor
+    )
+
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT `description` FROM `todos`")
+    results = cursor.fetchall()
     if request.method == 'POST':
         new_todo = request.form["new_todo"]
         todos.append(new_todo)
-    return render_template("todo.html.jinja", my_todos = todos)
+    return render_template("todo.html.jinja", my_todos = results)
 
 
 @app.route('/delete_todo/<int:todo_index>', methods = ['POST'])
